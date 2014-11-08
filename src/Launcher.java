a2198c1ea9df00a66e2506c7992acf592503e791
Enifs <d.romanovskis@gmail.com>
2014-11-08 23:53:13 +0200

    Created a basic draft for the main ui.

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,8 +1,27 @@
+import ui.MainUI;
+
+import javax.swing.*;
+
+
 /**
  * Created by Enifs.
  */
 
 public class Launcher
 {
+	public static void main(String[] args)
+	{
+		Launcher launcher = new Launcher();
+		launcher.run();
+	}
 
+	private void run()
+	{
+		MainUI ui = new MainUI();
+		JFrame frame = new JFrame("TBT");
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setContentPane(ui.getRootPanel());
+		frame.setVisible(true);
+		frame.setSize(800, 600);
+	}
 }
---
+++ src/ui/MainUI.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Enifs.
+ */
+
+package ui;
+
+import javax.swing.*;
+
+
+public class MainUI
+{
+
+	public JPanel getRootPanel()
+	{
+		return rootPanel;
+	}
+
+	private JPanel rootPanel;
+}
