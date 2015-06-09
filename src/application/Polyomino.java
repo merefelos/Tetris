c361df82417ad47724d77df87e050fc034b27bea
Anna Vanaga <annavanaga@gmail.com>
2015-06-09 23:26:32 +0300

    add Polyomino contents - some

--- src/application/Polyomino.java
+++ src/application/Polyomino.java
@@ -11,5 +11,44 @@ import java.util.List;
 
 public class Polyomino
 {
-	public List<Cell> cells = new ArrayList<Cell>();
+	public Polyomino()
+	{
+		this.cells = new ArrayList<Cell>();
+	}
+
+	public void move(int option)
+	{
+		switch (option)
+		{
+			case DOWN:
+				for (int i = 0; i < this.cells.size(); i++)
+				{
+					this.cells.set(i, new Cell(this.cells.get(i).getX(), this.cells.get(i).getY() - 30));
+				}
+				break;
+			case LEFT:
+				for (int i = 0; i < this.cells.size(); i++)
+				{
+					this.cells.set(i, new Cell(this.cells.get(i).getX() - 30, this.cells.get(i).getY()));
+				}
+				break;
+			case RIGHT:
+				for (int i = 0; i < this.cells.size(); i++)
+				{
+					this.cells.set(i, new Cell(this.cells.get(i).getX() + 30, this.cells.get(i).getY()));
+				}
+				break;
+			default:
+				for (int i = 0; i < this.cells.size(); i++)
+				{
+					this.cells.set(i, new Cell(this.cells.get(i).getX(), this.cells.get(i).getY() - 30));
+				}
+				break;
+		}
+	}
+
+	public              List<Cell> cells = new ArrayList<Cell>();
+	public static final int        DOWN  = 0;
+	public static final int        LEFT  = 1;
+	public static final int        RIGHT = 2;
 }
